fa421f993b765ad72ca7f68b6de4fe1c93675451
Pavel Zaitsev <pavel@arslogi.ca>
2020-07-13 10:51:09 -0400

    Initial Commit

---
+++ boom/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  boom
+//
+//  Created by Pavel Zaitsev on 7/13/20.
+//  Copyright © 2020 Pavel Zaitsev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
